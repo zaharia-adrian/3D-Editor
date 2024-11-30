3cd1f5c9190b575a6c009838471bd6185b00244a
zaharia-adrian <zadrian530@gmail.com>
2024-11-30 19:09:51 +0200

    Add project files.

---
+++ src/3D-Editor.cpp
@@ -0,0 +1,31 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    
+    while (window.isOpen())
+    {
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+            //e
+
+
+        }
+
+        window.clear(sf::Color::Black);
+
+ 
+        window.display();
+    }
+
+    return 0;
+}
